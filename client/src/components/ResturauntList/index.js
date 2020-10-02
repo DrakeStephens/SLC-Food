9d8e7880bc1dd9bda1514a10d46c11143cd9d646
Drake Stephens <meuio56@gmail.com>
2020-10-01 20:13:14 -0600

    single resturaunt queries and mutations

---
+++ client/src/components/ResturauntList/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ResturauntList = ({ resturaunts, title }) => {
+  if (!resturaunts.length) {
+    return <h3>No Resturaunts Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {resturaunts &&
+        resturaunts.map(resturaunt => (
+          <div key={resturaunt._id} className="card mb-3">
+            <p className="card-header">
+              <Link
+                to={`/resturaunt/${resturaunt._id}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {resturaunt.resturauntName}
+              </Link>{' '}
+            </p>
+            <div className="card-body">          
+              <p>{resturaunt.url}</p>
+              <p>{resturaunt.description}</p>
+              <p className="mb-0">
+                MenuItems: {resturaunt.menuItemCount} || Click to{' '}
+                {resturaunt.menuItemCount ? 'see' : 'start'} the discussion!
+              </p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ResturauntList;
