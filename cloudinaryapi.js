1077b0a648c43e1afb4ca4e52863cae28d80dcc2
Matt M <matt.mcfadden14@gmail.com>
2020-10-09 21:43:05 -0600

    cloudinary needs work

--- cloudinaryapi.js
+++ cloudinaryapi.js
@@ -14,3 +14,34 @@
 cloudinary.v2.uploader.upload("https://www.example.com/mysample.jpg",
   { public_id: "sample_woman" }, 
   function(error, result) {console.log(result); });
+
+
+
+
+var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dyfsfpiol/upload';
+var CLOUDINARY_UPLOAD_PRESET = 'nse81ijw';
+
+var imgPreview = document.getElementById('img-preview');
+var fileUpload = document.getElementById('file-upload');
+
+fileUpload.addEventListener('change', function(event) {
+  var file = event.target.files[0];
+  var formData = new FormData();
+
+  formData.append('file', file);
+  formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+  axios({
+    url: CLOUDINARY_URL,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: formData
+  }).then(function(res) {
+    console.log(res);
+    imgPreview.src = res.data.secure_url;
+  }).catch(function(err) {
+    console.log(err);
+  });
+});
